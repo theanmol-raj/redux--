725c641eebdb46f56669a1ea28eefaef09d635d7
Anmol Raj <course.anmolraj@gmail.com>
2023-01-10 21:40:48 +0530

    Redux Implementation

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
-import { useState ,createContext ,useContext } from "react"
+import React from 'react'
+import { useSelector ,useDispatch } from 'react-redux'
 
-const Env = createContext () ;
-
-function C1(){
-  return <div>
-    <h1>C1</h1>
-    <C2  />
-  </div>
-}
-function C2(){
-  return <div>
-      <h1>C2</h1>
-      <C3  />
-  </div>
-}
-function C3(){
-  return <div>
-      <h1>C3</h1>
-      <C4  />
-  </div>
-}
-
-function C4(){
-  const [va ,va2] = useContext(Env);
-  
-  return <div>
-    {/* hi again {va} */}
-    <p>hi again {va} {va2}</p>
-  </div>
-}
 
 function App() {
-  const [va ,setVa] = useState("Anmol")
-  const [va2 ,setVa2] = useState("Raj")
+  const {counter} = useSelector(store => store)
+  // console.log(x)
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch ({ type : "Increment" })
+  }
+
+  const Add = (x) => {
+    dispatch ({ type : "ADD" ,payload : x })
+  }
+
   return (
-    <Env.Provider value={[va ,va2]} >
-      <div className="App">
-          <h1>Hello {va}</h1>
-          <C1 />
-        </div>
-    </Env.Provider>
-  );
+    <div>
+      <h1>My counter App</h1>
+      <h1>{counter}</h1>
+      <button onClick={increment}>Increment</button>
+
+      <button onClick={() => Add(5)}>ADD</button>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
